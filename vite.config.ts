e93c9881308ee3a683275651642d15bff4b48eee
徐均 <xu.jun.work@qq.com>
2024-10-22 17:09:24 +0800

    fix: 优化打包

--- vite.config.ts
+++ vite.config.ts
@@ -34,5 +34,27 @@ export default defineConfig({
   },
   server:{
     port:8084
-  }
+  },
+  build: {
+    sourcemap: false,
+    minify: "terser",
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1500,
+    reportCompressedSize:false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output:{
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split("/")[1].toString()
+          }
+      }
+    }
+    },
+  },
 })
